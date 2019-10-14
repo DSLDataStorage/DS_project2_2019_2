3d9ed25d6c2e72e6fe767209a2dee47c2fdd0ba3
DSLDataStorage <rlaghdwl96@gmail.com>
2019-10-13 18:45:41 -0700

    v1.1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "Manager.h"
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 using namespace std;
 
 int main() {
@@ -8,19 +9,30 @@ int main() {
 	manager.run("command.txt");
 }
 
-/* comment */
-/*
+
+/* 								=Comment= 
 	1. Print format 
-	Command : INSERT, PRINT_AVL, SEARCH_AVL, PRINT_GP
+		1-1) Command : INSERT, PRINT_AVL, SEARCH_AVL
 
-	cout<<"( "<<(locationID)<<", "<<(name)<<", "<<(country)<<endl;
+			 cout<<"( "<<(locationID)<<", "<<(name)<<", "<<(country)<<" )"<<endl;
 
-	Command : PRINT_MST
+		1-2) Command : PRINT_MST
 
-	cout<<"( "<<(City1)<<", "<<(City2)<<"), "<<(Weight)<<endl;
-	cout<<"Total: "<<(total)<<endl;
+			cout<<"( "<<(City1)<<", "<<(City2)<<" ), "<<(Weight)<<endl;
+			cout<<"Total: "<<(total)<<endl;
 
-	note) you can change (locationID), (name), ... any variable ,
-		but you can't change print format
 
+		1-3) Command : PRINT_GP
+
+			#inlude <iomanip.h>
+			cout.setf(ios::left);
+			for(//i to N){
+				for(//j to N){
+					cout<<setw(3)<<(data);
+				}
+			cout<<endl;
+			}
+
+			note) you can change (locationID), (name), (data) ... any variable ,
+			but you can't change print format			
 */
